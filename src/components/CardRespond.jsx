f62d0833e65ffae1b8cb9b4bd04af0cfbefae16e
oscar-2000 <oscareduvalenzuela@gmail.com>
2025-08-12 23:13:45 -0400

    Responsive user

--- src/components/CardRespond.jsx
+++ src/components/CardRespond.jsx
@@ -5,16 +5,16 @@ import CommentContext from "@context/CommentContext";
 export default function CardRespond({idComment, idCommentParent, username, type}) {
     const {currentUser, newReply, comments, showReplyComment} = useContext(CommentContext);
     return(
-        <div key={idComment} className="bg-white rounded-lg p-5 flex flex-col md:flex-row gap-4 justify-between items-center">
-            <img src={currentUser.image.png} alt={currentUser.username} className="h-12 w-auto" />
+        <div key={idComment} className="bg-white rounded-lg p-5 flex flex-row flex-wrap gap-4 justify-center items-center">
+            <img src={currentUser.image.png} alt={currentUser.username} className="h-12 order-2 md:order-1 w-auto" />
             <textarea 
                 placeholder={`@${username}`} 
                 name={`respond_comment_${idComment}`} 
                 id={`respond_comment_${idComment}`} 
-                className="border-[1px] borde-morado p-4 rounded-lg w-full resize-none"
+                className="border-[1px] borde-morado p-4 rounded-lg order-1 w-full md:order-2 md:flex-10/14 resize-none"
             >
             </textarea>
-            <div className="flex flex-row md:flex-col gap-3 justify-end w-auto">
+            <div className="flex flex-row md:flex-col gap-3 justify-end w-auto order-2 md:order-3">
                 <button onClick={() => showReplyComment(false)} className="bg-red-600 text-white py-3 px-4 uppercase text-md rounded-lg font-semibold cursor-pointer">Cancel</button>
                 <button onClick={() => newReply(idComment, idCommentParent, document.getElementById(`respond_comment_${idComment}`).value, type)} className="bg-morado py-3 px-4 text-white uppercase text-md rounded-lg font-semibold cursor-pointer">Update</button>
             </div>
--- src/components/CardUserComment.jsx
+++ src/components/CardUserComment.jsx
@@ -4,21 +4,15 @@ import CommentContext from "@context/CommentContext"
 export default function CardUserComment() {
     const {currentUser, newComment} = useContext(CommentContext);
     return(
-        <div className="bg-white rounded-lg p-5">
-            <div className="flex flex-row items-start gap-5">
-                <img src={currentUser.image.webp} alt={currentUser.username} className="h-12 w-auto" />
-                <div className="w-full">
-                    <div className="flex flex-row items-start justify-between gap-3 w-full">
-                        <textarea name={`new_comment_${currentUser.id}`} id={`new_comment_${currentUser.id}`} placeholder="Add a comment..." className="border-[1px] borde-morado p-4 rounded-lg w-full mt-3 resize-none">
-                        </textarea>
-                        <button onClick={() => {
-                                newComment(document.getElementById(`new_comment_${currentUser.id}`).value),
-                                document.getElementById(`new_comment_${currentUser.id}`).value = "";
-                            }
-                        } className="bg-morado py-3 px-4 text-white uppercase text-lg rounded-lg font-semibold cursor-pointer">Send</button>
-                    </div>
-                </div>
-            </div>
+        <div className="bg-white rounded-lg p-5 flex flex-row flex-wrap gap-4 justify-between items-center">
+            <img src={currentUser.image.webp} alt={currentUser.username} className="h-12 order-2 md:order-1 w-auto" />
+            <textarea name={`new_comment_${currentUser.id}`} id={`new_comment_${currentUser.id}`} placeholder="Add a comment..." className="border-[1px] borde-morado p-4 rounded-lg order-1 w-full md:order-2 md:flex-10/14 resize-none">
+            </textarea>
+            <button onClick={() => {
+                    newComment(document.getElementById(`new_comment_${currentUser.id}`).value),
+                    document.getElementById(`new_comment_${currentUser.id}`).value = "";
+                }
+            } className="bg-morado py-3 px-4 text-white uppercase text-lg rounded-lg font-semibold cursor-pointer order-2 md:order-3">Send</button>
         </div>
     )
 
